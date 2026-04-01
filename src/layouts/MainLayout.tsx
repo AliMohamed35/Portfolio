a20a127bd53b0bd3970f25557bbc6ca31b840290
Ali Mohamed <alimohamed351@outlook.com>
2026-04-01 17:36:19 +0200

    added sections

--- src/layouts/MainLayout.tsx
+++ src/layouts/MainLayout.tsx
@@ -1,14 +1,27 @@
-import Hero from "../components/Hero/HeroSection";
 import NavBar from "../components/NavBar";
+import Hero from "../components/Hero/HeroSection";
+import AboutSection from "../components/About/AboutSection";
+import ExperienceSection from "../components/Experience/ExperienceSection";
+import ProcessSection from "../components/Process/ProcessSection";
+import PortfolioSection from "../components/Portfolio/PortfolioSection";
+import ContactSection from "../components/Contact/ContactSection";
+import Footer from "../components/Footer/Footer";
 
-// header and footer
-function MainLayout(){
-    return(
-        <>
-        <NavBar/>
-        <Hero/>
-        </>
-    );
+function MainLayout() {
+  return (
+    <>
+      <NavBar />
+      <main>
+        <Hero />
+        <AboutSection />
+        <ExperienceSection />
+        <ProcessSection />
+        <PortfolioSection />
+        <ContactSection />
+      </main>
+      <Footer />
+    </>
+  );
 }
 
 export default MainLayout;
